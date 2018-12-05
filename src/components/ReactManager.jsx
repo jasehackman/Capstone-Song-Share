122c7011c56ac2440e26a279add036b7dee0268d
jasehackman <jasehackman@gmail.com>
2018-12-05 16:09:52 -0600

    download url post to console but am struggling to get it to update state

--- src/components/ReactManager.jsx
+++ src/components/ReactManager.jsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import NavBar from "./nav/NavBar.jsx"
 import ApplicationManager from "./ApplicationManager.jsx"
 import APICalls from "../modules/APICalls"
+import firebase from "firebase"
 
 
 
@@ -20,7 +21,10 @@ export default class ReactManager extends Component {
 
     playlists: [],
 
-    songs_playlists: []
+    songs_playlists: [],
+    uploader: 0,
+    uploadedFileName:"",
+    songDownloadURL:""
   }
 
 
@@ -51,8 +55,60 @@ export default class ReactManager extends Component {
 
   fileUploader = (e) => {
     let file = e.target.files[0];
-    console.log(file);
-  }
+    console.log(file.name);
+    let fileName = file.name
+    let uploadedSong = firebase.storage().ref(file.name)
+    let task = uploadedSong.put(file)
+    let songDownloadUrl = ""
+    task.on('state_changed', function(snapshot){
+      // Observe state change events such as progress, pause, and resume
+      // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
+      var progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+      console.log('Upload is ' + progress + '% done');
+      switch (snapshot.state) {
+        case firebase.storage.TaskState.PAUSED: // or 'paused'
+          console.log('Upload is paused');
+          break;
+        case firebase.storage.TaskState.RUNNING: // or 'running'
+          console.log('Upload is running');
+          break;
+      }
+    }, function(error) {
+      // Handle unsuccessful uploads
+    }, function() {
+      // Handle successful uploads on complete
+      // For instance, get the download URL: https://firebasestorage.googleapis.com/...
+      task.snapshot.ref.getDownloadURL().then(function(downloadURL) {
+        console.log(downloadURL)
+        songDownloadUrl = downloadURL})
+      })
+    console.log(songDownloadUrl)
+    this.setState({
+      uploadedFileName: fileName,
+      songDownloadURL: songDownloadUrl
+    })
+    };
+    // let songDownloadURL = APICalls.getSingleSong(uploadedSong)
+    // this.setState({
+    //   uploadedFileName: fileName,
+    //   songDownloadURL: songDownloadURL
+
+    // })
+    // task.on('state_changed',
+    //   function progress(snapshot) {
+    //     let percentage = (snapshot.bytesTransferred/snapshot.totalBytes) *100;
+    //     console.log(percentage)
+
+    //   }),
+    //   function error(err) {
+
+    //   },
+    //   function complete() {
+
+    //   }
+
+
+
   render() {
     if(this.state.pageLoaded)
     return (
