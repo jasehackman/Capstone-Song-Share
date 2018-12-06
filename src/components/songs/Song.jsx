67a2f071b34fc9a3dfa9ac7da44d67e02d7e56f8
jasehackman <jasehackman@gmail.com>
2018-12-06 13:03:41 -0600

    text fields are editable but don't save to the database

--- src/components/songs/Song.jsx
+++ src/components/songs/Song.jsx
@@ -4,7 +4,8 @@ import './song.css'
 export default class Song extends Component {
 
   state = {
-    songPlayerClicked: false
+    songPlayerClicked: false,
+    editSongClicked: false
   }
 
   songClicked = () => {
@@ -16,11 +17,34 @@ export default class Song extends Component {
   }
 
 
+  editSongClickedFunction = () => {
+    this.setState({editSongClicked: true})
+  }
+
 
 
   render(){
+    //edit button clicked
+    if(this.state.songPlayerClicked && this.state.editSongClicked){
+      return(
+        <div>
+          <h3 contentEditable="true">{this.props.song.title}</h3>
+
+          <audio controls src={this.props.song.downloadURL}></audio>
+          <div>
+            <h5>CoWriters</h5>
+            <p contentEditable="true">{this.props.song.coWriter}</p>
+            <h3>Lyric</h3>
+            <p contentEditable="true">{this.props.song.lyric}</p>
+            <button>Edit</button><button>Delete</button>
+          </div>
+          <img className="icon" alt="expand" src="images/iconfinder_collapse2_308968.svg"/>
+
+        </div>
+      )
+    }
     //if a song is clicked
-    if(this.state.songPlayerClicked){
+    else if(this.state.songPlayerClicked){
         return(
         <div onClick={()=>this.songUnClicked()} >
           <h3>{this.props.song.title}</h3>
@@ -30,14 +54,16 @@ export default class Song extends Component {
             <p>Cowriter:{this.props.song.coWriter}</p>
             <h3>Lyric</h3>
             <p>{this.props.song.lyric}</p>
-
+            <button onClick = {this.editSongClickedFunction}>Edit</button><button>Delete</button>
           </div>
           <img className="icon" alt="expand" src="images/iconfinder_collapse2_308968.svg"/>
 
         </div>
         )
 
-    }else{
+    }
+
+    else{
     return (
       <div onClick={()=>this.songClicked()}>
           <h3>{this.props.song.title}</h3>
