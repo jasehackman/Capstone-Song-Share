1cace41a16bc2981cc0c5f9405ff462f39c34090
jasehackman <jasehackman@gmail.com>
2018-12-10 16:13:06 -0600

    add styling to playlist header

--- src/components/playlists/PlaylistsView.jsx
+++ src/components/playlists/PlaylistsView.jsx
@@ -2,6 +2,7 @@ import React, { Component } from "react"
 import APICalls from "../../modules/APICalls";
 import Playlist from "./Playlist.jsx"
 import NewPlaylistModal from './NewPlaylistModal.jsx'
+import './playlist.css'
 
 export default class PlaylistsView extends Component {
 
@@ -11,7 +12,7 @@ export default class PlaylistsView extends Component {
 
     return (
       <React.Fragment>
-        <div className="d-flex">
+        <div className="d-flex playlistTitle sticky-top">
         <h1 className="display-4" >Playlists</h1>
         {/* Modal */}
         <NewPlaylistModal addPlaylist={this.props.addPlaylist} handleFieldChange={this.props.handleFieldChange}/>
