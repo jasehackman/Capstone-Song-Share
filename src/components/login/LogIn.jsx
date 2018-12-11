b22f407f28ba2d0f34bf1ed8a54c78a15309ee2a
jasehackman <jasehackman@gmail.com>
2018-12-11 14:35:49 -0600

    login bug fixed and login styled

--- src/components/login/LogIn.jsx
+++ src/components/login/LogIn.jsx
@@ -21,39 +21,50 @@ export default class LogIn extends Component {
   loginCheck = () => {
     APICalls.getAllFromJson("users")
       .then(users => {
-        users.forEach(user => {
-          console.log(user)
-          if (this.state.email === user.email && this.state.password === user.password) {
-            sessionStorage.setItem("id", user.id)
-            this.props.refreshData()
-          } else {
-            alert("boo")
-          }
+        let logUser = users.find(user =>
+          this.state.email === user.email && this.state.password === user.password
+        )
 
-        })
+        if (logUser) {
+          sessionStorage.setItem("id", logUser.id)
+          this.props.refreshData()
+        }
+
+        else {
+          alert("boo")
+        }
 
       })
+
   }
+
 
   render() {
     return (
       <div>
-        <h3>LogIn</h3>
-        <div className="form-group">
-          <label>Email</label>
+        <h1 className="display-4">LogIn</h1>
 
-          <input id="email" className="form-control m-2" type="email" onChange={(e) => this.handleFieldChange(e)} />
-
-          <label>Password</label>
+          {/* <h3>Email</h3> */}
+          <div className="input-group input-group-lg">
+        <div className="input-group-prepend">
+          <span className="input-group-text" id="inputGroup-sizing-lg">Email</span>
+        </div>
+          <input id="email" className="form-control m-2 " type="email"  onChange={(e) => this.handleFieldChange(e)} />
+          </div>
+          {/* <h3>Password</h3> */}
+          <div className="input-group input-group-lg">
+        <div className="input-group-prepend">
+          <span className="input-group-text" id="inputGroup-sizing-lg">Password</span>
+        </div>
           <input id="password" className="form-control m-2" type="password" onChange={(e) => this.handleFieldChange(e)} />
+          </div>
+          <button className="btn btn-primary  m-2 input-group-lg" placeholder = "password" onClick={() => this.loginCheck()} >Login</button>
 
-          <button className="btn btn-sm m-2"onClick={() => this.loginCheck()} >Login</button>
-
-          <Link to="/signup">Sign Up</Link>
+          <Link className="btn btn-success" to="/signup">Sign Up</Link>
 
         </div>
 
-      </div>
+
     )
   }
 
